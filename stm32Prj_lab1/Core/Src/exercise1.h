4c268cc137bc78daa74801b895201ca92b8f9da5
Vinh Phat <caovinhphat1904@gmail.com>
2024-09-10 15:50:47 +0700

    done ex1

--- stm32Prj_lab1/Core/Src/exercise1.h
+++ stm32Prj_lab1/Core/Src/exercise1.h
@@ -8,6 +8,12 @@
 #ifndef SRC_EXERCISE1_H_
 #define SRC_EXERCISE1_H_
 
+#include "main.h"
 
+#define LED_ON 1
+#define LED_OFF 0
+
+void exercise_init();
+void exercise1_run();
 
 #endif /* SRC_EXERCISE1_H_ */
