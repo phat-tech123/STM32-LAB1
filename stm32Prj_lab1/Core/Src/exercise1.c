5cdc30f88345d96588b1bf083f4a08023268a1c9
Vinh Phat <caovinhphat1904@gmail.com>
2024-09-10 16:29:20 +0700

    done ex2

--- stm32Prj_lab1/Core/Src/exercise1.c
+++ stm32Prj_lab1/Core/Src/exercise1.c
@@ -12,18 +12,17 @@
 void exercise_init(){
 
 }
-int led_status = LED_OFF;
 
+//exercise 1
+int led_status = LED_OFF;
 void led_on(){
 	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
 }
-
 void led_off(){
 	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
 }
-
 void exercise1_run(){
 	switch(led_status){
 	case LED_ON:
@@ -42,3 +41,56 @@ void exercise1_run(){
 		break;
 	}
 }
+
+
+//exercise 2
+void turn_red(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+}
+void turn_green(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+}
+void turn_yellow(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+}
+int count = 0;
+int led2_status = RED;
+void exercise2_run(){
+	switch(led2_status){
+	case RED:
+		if(count == 0) turn_red();
+		++count;
+		if(count == 5) {
+			led2_status = GREEN;
+			count = 0;
+		}
+		break;
+	case GREEN:
+		if(count == 0) turn_green();
+		++count;
+		if(count == 3){
+			led2_status = YELLOW;
+			count = 0;
+		}
+		break;
+	case YELLOW:
+		if(count == 0) turn_yellow();
+		++count;
+		if(count == 2){
+			led2_status = RED;
+			count = 0;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+
+//
--- stm32Prj_lab1/Core/Src/exercise1.h
+++ stm32Prj_lab1/Core/Src/exercise1.h
@@ -13,7 +13,12 @@
 #define LED_ON 1
 #define LED_OFF 0
 
+#define RED 2
+#define GREEN 3
+#define YELLOW 4
+
 void exercise_init();
 void exercise1_run();
+void exercise2_run();
 
 #endif /* SRC_EXERCISE1_H_ */
